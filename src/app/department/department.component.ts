ff8781145a0c59a6154b9c10b9d411d991af89f8
ayaatef11 <ayabadrin667@gmail.com>
2025-02-16 20:02:54 +0200

    update department component

--- src/app/department/department.component.ts
+++ src/app/department/department.component.ts
@@ -1,12 +1,18 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-department',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './department.component.html',
   styleUrl: './department.component.css'
 })
 export class DepartmentComponent {
-
+  departments = [
+    { id: 'tab-1', name: 'Cardiology', description: 'Quis excepturi porro totam sint earum quo nulla perspiciatis eius.', active: true },
+    { id: 'tab-2', name: 'Neurology', description: 'Voluptas vel esse repudiandae quo excepturi.', active: false },
+    { id: 'tab-3', name: 'Hepatology', description: 'Velit veniam ipsa sit nihil blanditiis mollitia natus.', active: false },
+    { id: 'tab-4', name: 'Pediatrics', description: 'Ratione hic sapiente nostrum doloremque illum nulla praesentium id', active: false }
+  ];
 }
