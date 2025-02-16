4a87d95827fd997dc6b1d865eb8a83e205bc6d9d
ayaatef11 <ayabadrin667@gmail.com>
2025-02-16 20:36:03 +0200

    update gallery component

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,12 +1,22 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-gallery',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.css'
 })
 export class GalleryComponent {
-
+  galleryImages = [
+    { src: '/img/gallery/gallery-1.jpg' },
+    { src: '/img/gallery/gallery-2.jpg' },
+    { src: '/img/gallery/gallery-3.jpg' },
+    { src: '/img/gallery/gallery-4.jpg' },
+    { src: '/img/gallery/gallery-5.jpg' },
+    { src: '/img/gallery/gallery-6.jpg' },
+    { src: '/img/gallery/gallery-7.jpg' },
+    { src: '/img/gallery/gallery-8.jpg' }
+  ];
 }
