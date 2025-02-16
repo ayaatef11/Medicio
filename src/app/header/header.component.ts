0332117bfcf910d286989ba323d05854b8b50435
ayaatef11 <ayabadrin667@gmail.com>
2025-02-16 21:42:04 +0200

    update header componnt

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,13 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+list=[1,2,3,4]
 }
