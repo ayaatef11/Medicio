f708b82445a6bf29308651648c2367666bd5cb0b
ayaatef11 <ayabadrin667@gmail.com>
2025-02-16 20:35:24 +0200

    update footer component

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,34 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+  usefulLinks = [
+    { name: 'Home', url: '#' },
+    { name: 'About us', url: '#' },
+    { name: 'Services', url: '#' },
+    { name: 'Terms of service', url: '#' },
+    { name: 'Privacy policy', url: '#' }
+  ];
 
+  ourServices = [
+    { name: 'Web Design', url: '#' },
+    { name: 'Web Development', url: '#' },
+    { name: 'Product Management', url: '#' },
+    { name: 'Marketing', url: '#' },
+    { name: 'Graphic Design', url: '#' }
+  ];
+  socialLinks = [
+    { name: 'Twitter', url: '#', icon: 'bx bxl-twitter' },
+    { name: 'Facebook', url: '#', icon: 'bx bxl-facebook' },
+    { name: 'Instagram', url: '#', icon: 'bx bxl-instagram' },
+    { name: 'Skype', url: '#', icon: 'bx bxl-skype' },
+    { name: 'LinkedIn', url: '#', icon: 'bx bxl-linkedin' }
+  ];
 }
