150a89aff03a82607cff64fabf8ba9176023968a
ayaatef11 <ayabadrin667@gmail.com>
2025-02-16 05:38:43 +0200

    generate project components

---
+++ src/app/featured-services/featured-services.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-featured-services',
+  standalone: true,
+  imports: [],
+  templateUrl: './featured-services.component.html',
+  styleUrl: './featured-services.component.css'
+})
+export class FeaturedServicesComponent {
+
+}
---
+++ src/app/frequent-questions/frequent-questions.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-frequent-questions',
+  standalone: true,
+  imports: [],
+  templateUrl: './frequent-questions.component.html',
+  styleUrl: './frequent-questions.component.css'
+})
+export class FrequentQuestionsComponent {
+
+}
