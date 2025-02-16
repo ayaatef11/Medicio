015bcfa4873d2e07ea4f5e8fbb24c195d4b85ec6
ayaatef11 <ayabadrin667@gmail.com>
2025-02-16 21:41:27 +0200

    update features componunts

--- src/app/features/features.component.ts
+++ src/app/features/features.component.ts
@@ -1,12 +1,19 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-features',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './features.component.html',
   styleUrl: './features.component.css'
 })
 export class FeaturesComponent {
-
+  socialLinks = [
+    { name: 'Twitter', url: '#', icon: 'bx bxl-twitter' },
+    { name: 'Facebook', url: '#', icon: 'bx bxl-facebook' },
+    { name: 'Instagram', url: '#', icon: 'bx bxl-instagram' },
+    { name: 'Skype', url: '#', icon: 'bx bxl-skype' },
+    { name: 'LinkedIn', url: '#', icon: 'bx bxl-linkedin' }
+  ];
 }
