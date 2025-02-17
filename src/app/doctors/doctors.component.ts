2761bfa92ec75a08887f571b9ab0e31eb0671d02
ayaatef11 <ayabadrin667@gmail.com>
2025-02-17 04:07:31 +0200

    add doctor model

--- src/app/doctors/doctors.component.ts
+++ src/app/doctors/doctors.component.ts
@@ -1,12 +1,27 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { Doctor } from '../Models/Doctor.model';
 
 @Component({
   selector: 'app-doctors',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './doctors.component.html',
   styleUrl: './doctors.component.css'
 })
 export class DoctorsComponent {
-
+  doctors: Doctor[] = [
+    new Doctor("Walter White", "Chief Medical Officer", "/img/doctors/doctors-1.jpg", [
+      { icon: "bi bi-twitter", url: "#" },
+      { icon: "bi bi-facebook", url: "#" },
+      { icon: "bi bi-instagram", url: "#" },
+      { icon: "bi bi-linkedin", url: "#" }
+    ]),
+    new Doctor("Sarah Johnson", "Anesthesiologist", "/img/doctors/doctors-2.jpg", [
+      { icon: "bi bi-twitter", url: "#" },
+      { icon: "bi bi-facebook", url: "#" },
+      { icon: "bi bi-instagram", url: "#" },
+      { icon: "bi bi-linkedin", url: "#" }
+    ])
+  ];
 }
