552d3d4e3bc5fbfa27c40059b2a0f76765c7d649
ayaatef11 <ayabadrin667@gmail.com>
2025-02-17 06:00:01 +0200

    convert features to class based

--- src/app/featured-services/featured-services.component.ts
+++ src/app/featured-services/featured-services.component.ts
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './featured-services.component.css'
 })
 export class FeaturedServicesComponent {
-
+  iconBoxes: IconBox[] = [
+    new IconBox('fas fa-heartbeat', 'Lorem Ipsum', 'Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi', 100),
+    new IconBox('fas fa-pills', 'Sed ut perspiciatis', 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore', 200),
+    new IconBox('fas fa-thermometer', 'Magni Dolores', 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia', 300),
+    new IconBox('fas fa-dna', 'Nemo Enim', 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis', 400)
+  ];
 }
