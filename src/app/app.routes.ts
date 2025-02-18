837830df08759d6a954e771ae52f46e5f8726fe5
ayaatef11 <ayabadrin667@gmail.com>
2025-02-18 19:59:19 +0200

    add project routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AppointmentComponent } from './appointment/appointment.component';
+import { ContactComponent } from './contact/contact.component';
+import { CountsComponent } from './counts/counts.component';
+import { CtaComponent } from './cta/cta.component';
+import { DepartmentComponent } from './department/department.component';
+import { DoctorsComponent } from './doctors/doctors.component';
+import { FeaturedServicesComponent } from './featured-services/featured-services.component';
+import { FeaturesComponent } from './features/features.component';
+import { FooterComponent } from './footer/footer.component';
+import { FrequentQuestionsComponent } from './frequent-questions/frequent-questions.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { ServicesComponent } from './services/services.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'/',component:AboutComponent},
+  {path:'/about',component:AboutComponent},
+  {path:'/appointment',component:AppointmentComponent},
+  {path:'/contact',component:ContactComponent},
+  {path:'/counts',component:CountsComponent},
+  {path:'/cta',component:CtaComponent},
+{path:'department',component:DepartmentComponent},
+{path:'/doctors',component:DoctorsComponent},
+{path:'/featuredServices',component:FeaturedServicesComponent},
+{path:'/features',component:FeaturesComponent},
+{path:'/footer',component:FooterComponent},
+{path:'/frequent-questions',component:FrequentQuestionsComponent},
+{path:'/gallery',component:GalleryComponent},
+{path:'/header',component:HeaderComponent},
+{path:'/hero',component:HeroComponent},
+{path:'/pricing',component:PricingComponent},
+{path:'/services',component:ServicesComponent},
+{path:'/testimonials',component:TestimonialsComponent}
+
+];
