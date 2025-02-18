a132e0a1bc1860480b6b185b6f9afcdddce99e1d
ayaatef11 <ayabadrin667@gmail.com>
2025-02-18 20:46:03 +0200

    add select function in the app component

--- main.js
+++ main.js
@@ -1,17 +1,12 @@
 
 
 (
+
   function() {
+
   "use strict";
 //header and navbar
-  const select = (el, all = false) => {
-    el = el.trim()
-    if (all) {
-      return [...document.querySelectorAll(el)]
-    } else {
-      return document.querySelector(el)
-    }
-  }
+ 
 
   const on = (type, el, listener, all = false) => {
     let selectEl = select(el, all)
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,19 @@ import { HeaderComponent } from "./header/header.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [ HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Medicio';
+
+    select = (el:string, all:boolean = false):HTMLElement | HTMLElement[] | null=> {
+    el = el.trim()
+    if (all) {
+      return Array.from(document.querySelectorAll(el) )as HTMLElement[];//return all elements that are identicalto el
+    } else {
+      return document.querySelector(el)  as HTMLElement | null;
+    }
+  }
 }
