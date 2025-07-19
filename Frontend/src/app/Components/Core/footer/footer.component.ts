1bf06ab73921437123519950e5431b9d8a6d3deb
ayaatef11 <ayabadrin667@gmail.com>
2025-07-19 12:37:15 +0300

    update footer

--- Frontend/src/app/Components/Core/footer/footer.component.ts
+++ Frontend/src/app/Components/Core/footer/footer.component.ts
@@ -8,17 +8,11 @@ import { RouterLink } from '@angular/router';
 @Component({
   standalone:true,
     selector: 'app-footer',
-    imports: [CommonModule,RouterLink], // Ensure CommonModule is imported
+    imports: [CommonModule], // Ensure CommonModule is imported
     templateUrl: './footer.component.html',
     styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
-  usefulLinks: UsefulLink[] = [
-    new UsefulLink('Home', '/home'),
-    new UsefulLink('About us', '/about'),
-    new UsefulLink('Services', '/services'),
-    new UsefulLink('Contact', '/contact')
-  ];
 
   ourServices: OurService[] = [
     new OurService('Web Design', '#'),
@@ -28,12 +22,6 @@ export class FooterComponent {
     new OurService('Graphic Design', '#')
   ];
 
-  socialLinks: SocialLink[] = [
-    new SocialLink('Twitter', 'https://twitter.com/', 'bx bxl-twitter'),
-    new SocialLink('Facebook', 'https://www.facebook.com/', 'bx bxl-facebook'),
-    new SocialLink('Instagram', 'https://www.instagram.com/', 'bx bxl-instagram'),
-    new SocialLink('LinkedIn', 'https://www.linkedin.com/', 'bx bxl-linkedin')
-  ];
   subscribe(){
     console.log('success');
   }
